5f620e851ba781c008eb705fef83655ecbd53192
Arbaz Khan <49393716+Arbazkhan4712@users.noreply.github.com>
2020-05-11 23:29:21 +0530

    Update corona.py

--- corona.py
+++ corona.py
@@ -7,10 +7,7 @@ r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
 
 data = r.json()
 text = f'Confirmed Cases : {data["cases"]} \nDeaths : {data["deaths"]} \nRecovered : {data["recovered"]}'
-case = data["cases"]
-deaths = data["deaths"]
-recovered = data["recovered"]
-
+		
 while True:
 	toast = ToastNotifier()
 	toast.show_toast("Covid-19 Notification",text ,duration=20)
